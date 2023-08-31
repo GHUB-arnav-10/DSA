bcc70ba66475f0de7644285769ea3388034fd6fd
GHUB-arnav-10 <93794942+GHUB-arnav-10@users.noreply.github.com>
2023-08-31 16:32:20 +0530

    Add files via upload
    Implementation  of queue and circular queue in C++

---
+++ queue1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MyQueue {
+    private:
+        // store data
+        vector<int> data;
+        // pointer for start position
+        int p_start;
+    public:
+        MyQueue() {p_start = 0;}
+        /** Insert element queue, Return true if the operation is successful. */
+        bool enQueue(int x) {
+            data.push_back(x);
+            return true;
+        }
+        /** Delete an element from the queue. Return true if the operation is successful. */
+        bool deQueue() {
+            if (isEmpty()) {
+                return false;
+            }
+            p_start++;
+            return true;
+        };
+        /** Get the front item from the queue. */
+        int Front() {
+            return data[p_start];
+        };
+        /** Checks whether the queue is empty or not. */
+        bool isEmpty()  {
+            return p_start >= data.size();
+        }
+};
+
+int main() {
+    MyQueue q;
+    q.enQueue(5);
+    q.enQueue(3);
+    if (!q.isEmpty()) {
+        cout << q.Front() << endl;
+    }
+    q.deQueue();
+    if (!q.isEmpty()) {
+        cout << q.Front() << endl;
+    }
+    q.deQueue();
+    if (!q.isEmpty()) {
+        cout << q.Front() << endl;
+    }
+}
+
+
+
