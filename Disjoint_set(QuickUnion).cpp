bd25ebcf6b59aa65b1188ecadc68e137f64f1c3e
GHUB-arnav-10 <93794942+GHUB-arnav-10@users.noreply.github.com>
2023-09-06 22:29:17 +0530

    Using quick union instead
    Better than find as here worst case time complexity is O(n)

---
+++ Disjoint_set(QuickUnion).cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+class UnionFind {
+public:
+    UnionFind(int sz) : root(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+        }
+    }
+
+    int find(int x) {
+        while (x != root[x]) {
+            x = root[x];
+        }
+        return x;
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            root[rootY] = rootX;
+        }
+    }
+
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+
+private:
+    vector<int> root;
+};
+
+int main() {
+
+    cout << boolalpha;
+    UnionFind uf(10);
+    // 1-2-5-6-7 3-8-9 4
+    uf.unionSet(1, 2);
+    uf.unionSet(2, 5);
+    uf.unionSet(5, 6);
+    uf.unionSet(6, 7);
+    uf.unionSet(3, 8);
+    uf.unionSet(8, 9);
+    cout << uf.connected(1, 5) << endl;
+    cout << uf.connected(5, 7) << endl;
+    cout << uf.connected(4, 9) << endl;
+    // 1-2-5-6-7 3-8-9-4
+    uf.unionSet(9, 4);
+    cout << uf.connected(4, 9) << endl;
+
+    return 0;
+}
